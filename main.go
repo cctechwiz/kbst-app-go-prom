4f0ea8b18210ef2b1e4610f764f44026903175f3
Josh Maxwell <cctechwiz@gmail.com>
2021-11-13 13:49:47 +0000

    Modified metrics prefix to match app name

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func recordMetrics() {
 
 var (
 	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
+		Name: "app-go-prom_processed_ops_total",
 		Help: "The total number of processed events",
 	})
 )
